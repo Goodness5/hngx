1af1651ecf113601db9ac92d3e15bb2fbf925ca0
goodness5 <goodnesskolapo@gmail.com>
2023-09-29 17:19:24 +0100

    fuck it

--- helpmeout/inject.js
+++ helpmeout/inject.js
@@ -1,49 +1,5 @@
-document.getElementById('startRecording').addEventListener('click', startRecording);
-document.getElementById('stopRecording').addEventListener('click', stopRecording);
-document.getElementById('takeScreenshot').addEventListener('click', takeScreenshot);
-
-let mediaStream = null;
-let isRecording = false;
-let startTime = null;
-
-function startRecording() {
-  chrome.desktopCapture.chooseDesktopMedia(['screen'], (streamId) => {
-    if (!streamId) return;
-    navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: {
-        mandatory: {
-          chromeMediaSource: 'desktop',
-          chromeMediaSourceId: streamId,
-          minWidth: 1280,
-          maxWidth: 1280,
-          minHeight: 720,
-          maxHeight: 720
-        }
-      }
-    }).then((stream) => {
-      mediaStream = stream;
-      isRecording = true;
-      startTime = new Date();
-    }).catch((error) => {
-      console.error('Error starting recording:', error);
-    });
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({ url: chrome.runtime.getURL('controls/ontrols.html'), width: 250, height: 150 });
   });
-}
-
-function stopRecording() {
-  if (mediaStream) {
-    mediaStream.getTracks().forEach(track => track.stop());
-  }
-  isRecording = false;
-  startTime = null;
-}
-
-function takeScreenshot() {
-  chrome.tabs.captureVisibleTab(null, { format: 'png' }, (dataUrl) => {
-    const a = document.createElement('a');
-    a.href = dataUrl;
-    a.download = `screenshot_${new Date().getTime()}.png`;
-    a.click();
-  });
-}
+  
+  
--- helpmeout/popup.js
+++ helpmeout/popup.js
@@ -1,58 +1,111 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const startRecordingButton = document.getElementById('startRecording');
-    const stopRecordingButton = document.getElementById('stopRecording');
-    const takeScreenshotButton = document.getElementById('takeScreenshot');
-  
-    startRecordingButton.addEventListener('click', startRecording);
-    stopRecordingButton.addEventListener('click', stopRecording);
-    takeScreenshotButton.addEventListener('click', takeScreenshot);
-  });
-  
-  let mediaStream = null;
-  let mediaRecorder = null;
-  let recordedChunks = [];
-  
-  function startRecording() {
-    navigator.mediaDevices.getDisplayMedia({ video: true, audio: false })
-      .then((stream) => {
-        mediaStream = stream;
-        mediaRecorder = new MediaRecorder(stream);
-        mediaRecorder.ondataavailable = (event) => {
-          if (event.data.size > 0) {
-            recordedChunks.push(event.data);
-          }
-        };
-        mediaRecorder.onstop = () => {
-          const blob = new Blob(recordedChunks, { type: 'video/webm' });
-          const url = URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.href = url;
-          a.download = `recorded-${Date.now()}.webm`;
-          a.click();
-          recordedChunks = [];
-        };
-        mediaRecorder.start();
-      })
-      .catch((error) => {
-        console.error('Error starting recording:', error);
-      });
-  }
-  
-  function stopRecording() {
-    if (mediaRecorder) {
-      mediaRecorder.stop();
-    }
-    if (mediaStream) {
-      mediaStream.getTracks().forEach(track => track.stop());
-    }
-  }
-  
-  function takeScreenshot() {
-    chrome.tabs.captureVisibleTab(null, { format: 'png' }, (dataUrl) => {
-      const a = document.createElement('a');
-      a.href = dataUrl;
-      a.download = `screenshot_${new Date().getTime()}.png`;
-      a.click();
+let mediaRecorder;
+let stream;
+let chunks = [];
+let countdownValue = 2;
+let countdownInterval;
+
+const countdownElement = document.getElementById('countdown');
+const controlsElement = document.getElementById('controls');
+const startButton = document.getElementById('recording-toggle');
+const pauseButton = document.getElementById('pauseButton');
+const resumeButton = document.getElementById('resumeButton');
+const stopButton = document.getElementById('stopButton');
+
+
+function startScreenRecording() {
+    window.create({ url: chrome.runtime.getURL('controls/ontrols.html'), width: 250, height: 150 });
+    navigator.mediaDevices.getDisplayMedia({ video: true })
+        .then((s) => {
+            stream = s;
+            const options = {
+                audioBitsPerSecond: 128000,
+                videoBitsPerSecond: 2500000,
+                mimeType: "video/mp4",
+              };
+    
+            mediaRecorder = new MediaRecorder(stream, options)
+    
+            mediaRecorder.ondataavailable = (event) => {
+                
+                if (event.data.size > 0) {
+                    chunks.push(event.data);
+                }
+            }
+    
+            mediaRecorder.onstop = () => {
+                const blob = new Blob(chunks, { type: 'video/webm' });
+    
+                // Save the recording
+                const url = URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = 'screen-recording.webm';
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+    
+                chunks = [];
+            }
+        })
+        .catch((err) => {
+            console.error('Error accessing screen:', err);
+        });
+
+
+
+    countdownInterval = setInterval(() => {
+        countdownElement.innerText = countdownValue;
+        if (countdownValue === 0) {
+            clearInterval(countdownInterval);
+            mediaRecorder.start();
+        }
+        countdownValue--;
+    }, 1000);
+
+    controlsElement.style.display = 'block';
+
+    pauseButton.addEventListener('click', () => {
+        mediaRecorder.pause();
+        pauseButton.disabled = true;
+        resumeButton.disabled = false;
     });
-  }
-  
+
+    resumeButton.addEventListener('click', () => {
+        mediaRecorder.resume();
+        pauseButton.disabled = false;
+        resumeButton.disabled = true;
+    });
+
+    stopButton.addEventListener('click', () => {
+        stopRecording();
+    });
+
+    startButton.disabled = true;
+}
+
+function stopRecording() {
+    mediaRecorder.stop();
+    stream.getVideoTracks()[0].stop();
+    clearInterval(countdownInterval);
+}
+
+function takeScreenshot() {
+    chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
+        const a = document.createElement("a");
+        a.href = dataUrl;
+        a.download = `screenshot_${new Date().getTime()}.png`;
+        a.click();
+    });
+}
+
+document
+    .getElementById("recording-toggle")
+    .addEventListener("click", startScreenRecording);
+
+document
+    .getElementById("stopButton")
+    .addEventListener("click", stopRecording);
+
+document
+    .getElementById("takeScreenshot")
+    .addEventListener("click", takeScreenshot);
