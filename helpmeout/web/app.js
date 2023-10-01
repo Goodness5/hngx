194c05bbf7a5bf85198db3a067a498902732b99a
goodness5 <goodnesskolapo@gmail.com>
2023-10-01 22:26:57 +0100

    omoooo

--- helpmeout/web/app.js
+++ helpmeout/web/app.js
@@ -19,58 +19,41 @@ const copyUrl = () => {
 };
 
 document.addEventListener("DOMContentLoaded", function() {
-    const params = new URLSearchParams(window.location.search);
-    const videourl = params.get("videoUrl");
-
-    if (videourl) {
-        videoUrl.value = videourl;
-
-        const playButton = document.querySelector('.play-button');
-        playButton.addEventListener('click', function() {
-            if (video.paused) {
-                video.play();
-            } else {
-                video.pause();
-            }
-        });
+        const params = new URLSearchParams(window.location.search);
+        const videourl = params.get("videoUrl");
     
-        // Add event listener for volume button
-        const volumeButton = document.querySelector('.volume-button');
-        volumeButton.addEventListener('click', function() {
-            if (video.muted) {
-                video.muted = false;
-            } else {
-                video.muted = true;
-            }
-        });
+        if (videourl) {
+            videoUrl.value = videourl;
     
-        // Add event listener for settings button (you can implement your own settings functionality)
-        const settingsButton = document.querySelector('.settings-button');
-        settingsButton.addEventListener('click', function() {
-            // Add your settings functionality here
-        });
+            var video = document.getElementById('videoPlayer');
+            var source = document.createElement('source');
     
-        // Update the time display
-        video.addEventListener('timeupdate', function() {
-            const currentTime = formatTime(video.currentTime);
-            const duration = formatTime(video.duration);
-            const timeDisplay = document.querySelector('.time-display');
-            timeDisplay.textContent = `${currentTime} / ${duration}`;
-        });
+            source.setAttribute('src', videourl);
+            source.setAttribute('type', 'video/mp4');
     
-        function formatTime(seconds) {
-            const minutes = Math.floor(seconds / 60);
-            seconds = Math.floor(seconds % 60);
-            return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+            video.appendChild(source);
+            video.play();
         }
+    });
     
-        var video = document.getElementById('video');
-        var source = document.createElement('source');
-        
-        source.setAttribute('src', videourl);
-        source.setAttribute('type', 'video/mp4');
-        
-        video.appendChild(source);
-        video.play();
-}
-});
+    function togglePlay() {
+        var video = document.getElementById('videoPlayer');
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    }
+    
+    document.getElementById('videoPlayer').addEventListener('timeupdate', function() {
+        var video = this;
+        var currentTimeElement = document.getElementById('currentTime');
+        currentTimeElement.innerText = formatTime(video.currentTime);
+    });
+    
+    function formatTime(seconds) {
+        var minutes = Math.floor(seconds / 60);
+        var remainingSeconds = Math.floor(seconds % 60);
+        return minutes + ':' + (remainingSeconds < 10 ? '0' : '') + remainingSeconds;
+    }
+    
