0acc9c406e7e32c56e050dcd49864c4ab45ec259
goodness5 <goodnesskolapo@gmail.com>
2023-10-02 18:33:41 +0100

    chai

--- page_assistant/src/app/page.js
+++ page_assistant/src/app/page.js
@@ -1,17 +1,24 @@
+"use client"
 import React, { useState } from 'react';
 import HumanAnimation from './components/animation';
+import video1 from './video1.mp4'; // Import your video files
+import video2 from './video2.mp4';
+import video3 from './video3.mp4';
 
 const Home = () => {
   const [explanation, setExplanation] = useState('');
   const [animationVisible, setAnimationVisible] = useState(false);
+  const [currentVideo, setCurrentVideo] = useState(null);
 
-  const handleHover = (text) => {
+  const handleHover = (text, video) => {
     setExplanation(text);
+    setCurrentVideo(video);
     setAnimationVisible(true);
   }
 
   const handleLeave = () => {
     setExplanation('');
+    setCurrentVideo(null);
     setAnimationVisible(false);
   }
 
@@ -21,30 +28,33 @@ const Home = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <div 
           className="hoverable-element p-4 bg-white dark:bg-gray-800 rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
-          onMouseEnter={() => handleHover('Explanation 1')}
+          onMouseEnter={() => handleHover('Explanation 1', video1)}
           onMouseLeave={handleLeave}
         >
           <h2 className="text-xl mb-4">Element 1</h2>
         </div>
         <div 
           className="hoverable-element p-4 bg-white dark:bg-gray-800 rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
-          onMouseEnter={() => handleHover('Explanation 2')}
+          onMouseEnter={() => handleHover('Explanation 2', video2)}
           onMouseLeave={handleLeave}
         >
           <h2 className="text-xl mb-4">Element 2</h2>
         </div>
         <div 
           className="hoverable-element p-4 bg-white dark:bg-gray-800 rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
-          onMouseEnter={() => handleHover('Explanation 3')}
+          onMouseEnter={() => handleHover('Explanation 3', video3)}
           onMouseLeave={handleLeave}
         >
           <h2 className="text-xl mb-4">Element 3</h2>
         </div>
       </div>
-      {explanation && <div className="text-center mt-4">{explanation}</div>}
-      {animationVisible && <div className="text-center fixed bottom-4 left-1/2 transform -translate-x-1/2">
-        <HumanAnimation />
-      </div>}
+      {explanation && <div className="text-center mt-4 bottom-0 absolute">{explanation}</div>}
+      {animationVisible && 
+        <div className="text-center fixed bottom-4 left-1/2 transform -translate-x-1/2">
+          <HumanAnimation />
+          {currentVideo && <video src={currentVideo} autoPlay loop muted />}
+        </div>
+      }
     </div>
   );
 };
